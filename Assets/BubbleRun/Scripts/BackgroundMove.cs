4c4035a2ec42964840b6f6c203ff85e8e629891d
Sage Tesoro <satewarco@gmail.com>
2025-01-25 16:29:53 -0700

    ok

---
+++ Assets/BubbleRun/Scripts/BackgroundMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMove : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
