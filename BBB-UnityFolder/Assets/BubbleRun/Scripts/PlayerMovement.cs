a8101cbd4be48f626bae581c9fc283d346309ea5
Sage Tesoro <satewarco@gmail.com>
2025-01-25 14:38:07 -0700

    tired

--- BBB-UnityFolder/Assets/BubbleRun/Scripts/PlayerMovement.cs
+++ BBB-UnityFolder/Assets/BubbleRun/Scripts/PlayerMovement.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Vector2 boundaryMin;
+    [SerializeField] private Vector2 boundaryMax;
     private void Update()
     {
+        // get player input
+        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float moveY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         Vector2 inputVector = new Vector2(0, 0);
         if (Input.GetKey(KeyCode.W))
         {
@@ -28,6 +33,11 @@ public class PlayerMovement : MonoBehaviour
         inputVector = inputVector.normalized;
 
         Vector3 moveDir = new Vector3(-(inputVector.x), -(inputVector.y),0f);
-        transform.position += moveDir *Time.deltaTime * moveSpeed;
+        
+        // move background
+        //Vector3 moveBackground = transform.position + new Vector3(moveX, moveY, 0f);
+
+        // clamp background (cannot move outside boundries)
+        //transform.position += moveBackground
     }
 }
